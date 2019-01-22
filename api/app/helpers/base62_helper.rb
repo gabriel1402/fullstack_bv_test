b9868b422829bf3b8ac0736317a49a5b48555b32
Gabriel Rojas <gabrielrojas_a@hotmail.com>
2019-01-21 22:45:31 -0600

    Added helper class for base62 encoding

---
+++ api/app/helpers/base62_helper.rb
@@ -0,0 +1,23 @@
+module Base62_Helper
+  
+  KEYS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.freeze
+
+  def encode(id)
+    str = ''
+    while id > 0
+      str.prepend KEYS[id % KEYS.size]
+      id = id / KEYS.size
+    end
+    return str
+  end
+
+  def decode(str)
+    
+    id = 0
+    str.chars.reverse.each_with_index do |char, index|
+      position = KEYS.size ** index
+      id = id + KEYS.index(char) * position
+    end
+    return id
+  end
+end
