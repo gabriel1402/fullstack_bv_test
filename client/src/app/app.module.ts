ed94d46f5ec736f34d893a017ac897f7805c8d40
Gabriel Rojas <gabrielrojas_a@hotmail.com>
2019-01-30 21:45:25 -0600

    Added navigation bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UrlShortenerComponent } from './url-shortener/url-shortener.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UrlShortenerComponent
+    UrlShortenerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
